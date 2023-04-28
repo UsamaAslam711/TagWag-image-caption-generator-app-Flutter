3f625d77f6826414508832c3ff6dd0d10e86a98c
Muhammad Usama <itxusama711@gmail.com>
2023-04-29 00:00:09 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'BottomNavigationBar.dart';
+import 'HelpScreen.dart';
+import 'SlideScreen.dart';
+import 'home.dart';
+import 'splashscreen.dart';
+
+void main() {
+  // Add this line
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TagWag',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/home': (context) => OnbodingScreen(),
+      },
+      // use SplashPage as the home widget
+    );
+  }
+}
