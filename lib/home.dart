3a43b8be751ddc1512e9f56bcc1b3d672b319f26
Muhammad Usama <itxusama711@gmail.com>
2023-07-07 23:38:13 +0500

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,3 @@
-                                      MediaQuery.of(context).size.width - 180,
-                                  alignment: Alignment.center,
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 24, vertical: 17),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    border: Border.all(
-                                      color: Color.fromARGB(255, 255, 255, 255),
-                                      width: 2,
-                                    ),
-                                    gradient: LinearGradient(
                                       colors: [
                                         Color.fromARGB(255, 70, 252, 243),
                                         Color.fromARGB(255, 121, 73, 255),
