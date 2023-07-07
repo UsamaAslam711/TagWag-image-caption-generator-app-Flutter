8b851dfa35c1f711ea66c29d94a15fcd1ab8724b
Muhammad Usama <itxusama711@gmail.com>
2023-07-07 23:38:26 +0500

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,3 @@
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 24, vertical: 17),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    border: Border.all(
-                                      color: Color.fromARGB(255, 255, 255, 255),
-                                      width: 2,
-                                    ),
-                                    gradient: LinearGradient(
-                                      colors: [
-                                        Color.fromARGB(255, 70, 252, 243),
-                                        Color.fromARGB(255, 121, 73, 255),
-                                        Color.fromARGB(255, 203, 73, 255)
-                                      ],
                                       begin: Alignment.topLeft,
                                       end: Alignment.bottomRight,
                                     ),
