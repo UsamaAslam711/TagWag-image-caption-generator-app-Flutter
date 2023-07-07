be2aa15daeb6cc7d61d0a678a8ef82c679b33065
Muhammad Usama <itxusama711@gmail.com>
2023-07-07 23:38:36 +0500

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,36 +1,3 @@
-                                      horizontal: 34, vertical: 17),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    border: Border.all(
-                                      color: Color.fromARGB(255, 255, 255, 255),
-                                      width: 2,
-                                    ),
-                                    gradient: LinearGradient(
-                                      colors: [
-                                        Color.fromARGB(255, 70, 252, 243),
-                                        Color.fromARGB(255, 121, 73, 255),
-                                        Color.fromARGB(255, 203, 73, 255)
-                                      ],
-                                      begin: Alignment.topLeft,
-                                      end: Alignment.bottomRight,
-                                    ),
-                                  ),
-                                  child: Text(
-                                    'Take a Photo',
-                                    style: TextStyle(
-                                      color: Color.fromARGB(255, 255, 255, 255),
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
                 )
               ],
             ),
